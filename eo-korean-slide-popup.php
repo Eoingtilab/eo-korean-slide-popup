8105ad24fec021c775a1eb315cec72a11c709313
Eoingti Lab Inc. <master@eoingti.com>
2026-06-25 02:46:29 +0900

    Bump plugin version to 1.2.3

--- eo-korean-slide-popup.php
+++ eo-korean-slide-popup.php
@@ -3,7 +3,7 @@
  * Plugin Name: EO Korean Slide Popup
  * Plugin URI: https://eoingti.com/
  * Description: 가볍고 테마 충돌이 적은 한국형 슬라이드 팝업 플러그인. 이미지, 영상, HTML, KBoard URL 카드를 지원합니다.
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: EOINGTI Lab
  * Author URI: https://eoingti.com/
  * Text Domain: eo-korean-slide-popup
@@ -15,7 +15,7 @@ if (!defined('ABSPATH')) {
 	exit;
 }
 
-define('EOKSP_VERSION', '1.2.2');
+define('EOKSP_VERSION', '1.2.3');
 define('EOKSP_FILE', __FILE__);
 define('EOKSP_PATH', plugin_dir_path(__FILE__));
 define('EOKSP_URL', plugin_dir_url(__FILE__));
